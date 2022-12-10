cef38e420ace4f793a5481eb928ba5f33faa03fd
German Lopez Pineda <me1lopig@uco.es>
2022-12-10 14:05:45 +0100

    fase de explotacion

--- Cv/calculoCVSivaram.py
+++ Cv/calculoCVSivaram.py
@@ -4,20 +4,26 @@
 # The Japanese Geotechnical Society
 # September 1976
 
+# programado por Germán López Pineda
+# Ingeniero de Caminos, Canales y Puertos UGR
+# Master en Matemática Computacional UJI
+# MAster en Ingeniería del Terreno UCO
+
+
 import math
 
 # entrada de los datos del ensayo
 
 d1=float(input("Lectura 1 de la zona de inicio de la consolidacion "))
-t1=float(input("Tiempo 1 de la zona de inicio de la consolidacion "))
+t1=float(input("Tiempo 1 de la zona de inicio de la consolidacion [sg]="))
 
 d2=float(input("Lectura 2 de la zona de inicio de la consolidacion "))
-t2=float(input("Tiempo 2 de la zona de inicio de la consolidacion "))
+t2=float(input("Tiempo 2 de la zona de inicio de la consolidacion [sg]="))
 
 d3=float(input("Lectura 3 de la zona final de la consolidacion "))
-t3=float(input("Tiempo 3 de la zona final de la consolidacion "))
+t3=float(input("Tiempo 3 de la zona final de la consolidacion [sg]="))
 
-hd=float(input("Distancia de drenaje "))
+hd=float(input("Distancia de drenaje [cm]"))
 
 
 # procesado de los datos cálculos intermedios
@@ -30,5 +36,5 @@ cv=math.pi*0.25*((d1-d2)*hd/((rt-rf)*(math.sqrt(t2)-math.sqrt(t1))))**2
 # impresion de resultados
 print("Rt=",rt)
 print("Rf=",rf)
-print("Cv=",cv)
+print("Cv=",cv," cm^2/sg")
 
