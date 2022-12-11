338bf62dd34e24e3d4b15c2438a31b902d1f2dcd
German Lopez Pineda <me1lopig@uco.es>
2022-12-11 22:38:29 +0100

    20221211

--- Cv_correlacion/Cv_correlacon.py
+++ Cv_correlacion/Cv_correlacon.py
@@ -1,3 +1,14 @@
 # cálculo del valor de Cv mediante correlaciones
 # poner aqui la referecnia del artículo 
 
+
+print("[1] Muestras remoldeadas ")
+print("[2] Muestras inalteradas en la rama de compresión ")
+print("[3] Muestras inalteradas en la rama de recompresión ")
+print("Otra entrada->Salir del programa ")
+
+tipo=int(input("Introducir tipo de cálculo a realizar->"))
+
+
+
+LL=float(input("Valor del LL [%]="))
